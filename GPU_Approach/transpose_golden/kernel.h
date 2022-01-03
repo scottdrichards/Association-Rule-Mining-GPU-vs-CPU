984975a6f4e7b8bb94c5ccb049ff6d2b4dcea754
scottdrichards <scottdrichards@hotmail.com>
2022-01-02 22:01:00 -0500

    clean up and organize

--- GPU_Approach/transpose_golden/kernel.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <cuda.h>
-
-__global__ void cudaTranspose(cudaPitchedPtr db_gpuPitchPtr, dim3 db_gpuDim, cudaPitchedPtr db_tran_gpuPitchPtr);
-
